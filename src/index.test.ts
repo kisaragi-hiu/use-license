92b33e117e0120535c93db043d35f9ae421f4ba4
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-22 22:41:22 +0900

    Fix index.ts not being included in coverage denominator

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "bun:test"
 import { getLicense, getLicenseList } from "./get.ts"
 
+// To actually register index.ts and other files for consideration
+// Otherwise 0% tested is treated as 100% tested
+import "./index.ts"
+
 test("getting and parsing license list", async () => {
   expect(await getLicenseList()).toEqual(expect.anything())
 })
